8e5723af3de804a82919c0e6c8643ad02b665990
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2020-08-25 02:28:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# polaroid-gallery
+
+A single-page website showcasing photos I've taken on my iPhone 8+ using the HUJI app.
+
+Technology Used: HTML5, CSS3, Vanilla JavaScript
+Tutorial followed: https://www.youtube.com/watch?v=4SQXOA8Z-lo
---
+++ main.js
@@ -0,0 +1,25 @@
+const modal = document.querySelector(".modal"),
+      previews = document.querySelectorAll(".photo img"),
+      originalImg = document.querySelector(".full-img"),
+      caption = document.querySelector(".caption");
+
+previews.forEach((preview) => {
+    preview.addEventListener('click', () => {
+        modal.classList.add("modal-open");
+        originalImg.classList.add("img-open");
+
+        // dynamically change text and image
+        const originalSrc = preview.getAttribute('data-original');
+        originalImg.src = `./assets/${originalSrc}`;
+        
+        const altText = preview.alt;
+        caption.textContent = altText;
+    });
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target.classList.contains('modal')) {
+        modal.classList.remove("modal-open");
+        originalImg.classList.remove("img-open");
+    }
+});
